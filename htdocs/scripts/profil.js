6e8cc01dda2c6898c20b573cb048de05b0ddd419
VBatto <vdkstar@gmail.com>
2021-10-24 20:09:10 +0200

    profile update

---
+++ htdocs/scripts/profil.js
@@ -0,0 +1,11 @@
+// STRICT MODE 
+'use strict';
+
+
+//TODO: if user = admin  ->  show "uzivatele" button (otherwise hide it)
+
+//TODO: filtering
+
+//TODO: page scroll OR loading next/pev page content
+
+//TODO: individual button functions
