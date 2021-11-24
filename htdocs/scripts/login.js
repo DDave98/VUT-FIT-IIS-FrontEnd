b19862309081808d81736db9781bfc1d4c1429a1
VBatto <vdkstar@gmail.com>
2021-11-24 18:15:37 +0100

    alert()-fixes

--- htdocs/scripts/login.js
+++ htdocs/scripts/login.js
@@ -1,112 +1,188 @@
-// button click event
-document.getElementById("btn").onclick = function() {formData(event)};              // login
-document.getElementById("btn2").onclick = function() {formDataREG(event)};          // register
+// button click event:
+document.getElementById("btn").onclick = function () { requiredField() };              // log in
+document.getElementById("btn2").onclick = function () { requiredFieldREG() };          // register
 
-// ----------------------------------------------------- for LOGIN ------------------------------------------------------------
-// Handling submitions from form (w\ FormData API):     
-function formData(event) 
+
+
+// --------------------------------------------------- Checking that the "required" fields are filled ----------------------------------------------
+// For LOG IN:
+
+function requiredField() 
 {
-    // prevents a page refresh
-    event.preventDefault();
-  
-    const data = new FormData(document.getElementById("formId"));
-  
-    // passing 'entries' method output to 'Object.fromEntries', which returns a plain js object -> to use 'JSON.stringify' later
-    const usrdata = Object.fromEntries(data.entries());
-
-
-    //(for LOG IN): array in ' name="checkbox" '  contains all checked values (if there are any: "on" or [none])
-    usrdata.keepSingnedIn= data.getAll("keepSingnedIn");
-
-
-    // take data from form ("group" class selection)
-    var jsonData = document.querySelector('.group');               
-
-    // converting values to JSON (= ready to be sent to a server)
-    jsonData = JSON.stringify(usrdata, null, 2);                    
-
-
-    //check email format (regex):
-    //TODO = call emailValidate(mail)
-
-
+  var empty = document.forms["form-login"]["email"].value;
+  if (empty == "") {
+    alert("Please fill in all fields!");
+    return false;
   }
-  
+
+  empty = document.forms["form-login"]["password"].value;
+  if (empty == "") {
+    alert("Please fill in all fields!");
+    return false;
+  }
+
+  // call fn to gather and work with the data
+  formData(event);
+
+  return true;
+}
 
 
 
-  // ---------------------------------------------- for SIGN UP (= same princip as with login fn) ---------------------------------------
-  function formDataREG(event) 
+
+// For SIGN UP:
+function requiredFieldREG() 
 {
-    event.preventDefault();
-  
-    const data = new FormData(document.getElementById("formIdREG"));
-  
-    const usrdataR = Object.fromEntries(data.entries());
-
-    //check email format (regex):
-    //TODO = call emailValidate(mail)
-
-    //compare password strings:   
-    //NOTE: REMOVE password_check before making the json!!!:      "delete usrdataR.password_check;"
-    //TODO
-
-
-
-
-    var jsonDataR = document.querySelector('.group2');               
-
-    jsonDataR = JSON.stringify(usrdataR, null, 2);                   
-
-
-
-
+  var empty = document.forms["form-reg"]["email"].value;
+  if (empty == "") {
+    alert("Please fill in all fields!");
+    return false;
   }
-  
 
-  // ---------- Checking that the "required" fields are filled ---------- (call onclick()!!)
-  //todo (implement into html, check functionality + test)
+  empty = document.forms["form-reg"]["username"].value;
+  if (empty == "") {
+    alert("Please fill in all fields!");
+    return false;
+  }
 
-  /* 
-   function requiredField(input) 
-   {
-     // if the lenght of the (element;s) string is 0 = field is 'empty'
-     if (input.value.length == 0)
-      { 
-         alert("message");  	
-         return false; 
-      }  	
-      return true; 
-    } 
-  */
+  empty = document.forms["form-reg"]["familyname"].value;
+  if (empty == "") {
+    alert("Please fill in all fields!");
+    return false;
+  }
+
+  empty = document.forms["form-reg"]["password"].value;
+  if (empty == "") {
+    alert("Please fill in all fields!");
+    return false;
+  }
+
+  //-----
+  empty = document.forms["form-reg"]["password_check"].value;
+  if (empty == "") {
+    alert("Please fill in all fields!");
+    return false;
+  }
+
+  // call fn to gather and work with the data
+  formDataREG(event);
+
+  return true;
+}
 
 
 
 
 
-  // ----------- Checking for valid email format -------------             (call from the other fns!!)
-  //todo (implement into json form functions above, check functionality + test)
+// ------------------------------------------------- Handling submitions from form (using FormData API) --------------------------------------------------------
+// For LOG IN:
 
-  /* 
-    function emailValidate(mail) 
+function formData(event)
+{
+  // prevents a page refresh
+  event.preventDefault();
+
+  const data = new FormData(document.getElementById("formId"));
+
+  // passing 'entries' method output to 'Object.fromEntries', which returns a plain js object -> to use 'JSON.stringify' later
+  const usrdata = Object.fromEntries(data.entries());
+
+
+  //array in ' name="checkbox" '  contains all checked values (if there are any: "on" or [none])
+  usrdata.keepSingnedIn = data.getAll("keepSingnedIn");
+
+
+  // take data from form ("group" class selection)
+  var jsonData = document.querySelector('.group');
+
+  // converting values to JSON (= ready to be sent to a server)
+  jsonData = JSON.stringify(usrdata, null, 2);
+
+
+  //check email format:
+  function emailValidate(mail);
+
+}
+
+
+
+
+// For SIGN UP:
+function formDataREG(event) 
+{
+  event.preventDefault();
+
+  const data = new FormData(document.getElementById("formIdREG"));
+
+  const usrdataR = Object.fromEntries(data.entries());
+
+  //check email format:
+  function emailValidate(mail);
+
+
+
+  //*
+
+
+  var jsonDataR = document.querySelector('.group2');
+
+  jsonDataR = JSON.stringify(usrdataR, null, 2);
+
+  //compare password strings:  
+  //( NOTE: REMOVE password_check before making the json!!!)
+  // - compare the 2 strings -> alert if wrong OR if ok "delete usrdataR.password_check;" -> again " jsonDataR = JSON.stringify(usrdataR, null, 2); "
+  //* - OR maybe (but not sure if it will work): 
+  //if (JSON.stringify(jsonDataR.password) === JSON.stringify(jsonDataR.password_check)) {} 
+  //else {alert("Passwords do not match!");}
+
+
+  //TODO
+
+
+}
+
+
+
+
+
+
+// --------------------------------------------------------- Checking for valid email format ---------------------------------------------------------------             
+  //(call from the other fns!!)
+  //todo ( check functionality + test)
+
+
+function emailValidate(mail)
+{
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
     {
-      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
-        {
-          return (true)
-        }
-          alert("You have entered an invalid email address!")
-          return (false)
+      return (true)
     }
-  */
+      alert("You have entered an invalid email address!")
+      return (false)
+}
+
+
+
+
+
+
+
 
 
 
 
 
 // __________________________________________________________________________________________________________________________________________
-// send to server:
+// sending JSON (request) to server:
 //TODO
 
+
+
+
+
+// receiving JSON (answer) from server:
+// ( NOTE: must turn back to js object to be able to use the data)
+//TODO
 
 
 
@@ -131,18 +207,18 @@ var request = new XMLHttpRequest()
 // Open a new connection, using the POST request on the URL endpoint
 request.open('POST', 'http://iis-proj.herokuapp.com/', true)
 
-request.onload = function () 
+request.onload = function ()
 {
   // Begin accessing JSON data here
 
     var data = JSON.parse(this.response)
 
-    
+
     data.forEach(movie => {
     // Log each movie's title
     console.log(movie.title)
     })
-    
+
 }
 
 // Send request
