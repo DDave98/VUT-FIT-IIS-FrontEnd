5567b750c3bafad83f17483775c626de26517a48
VBatto <vdkstar@gmail.com>
2021-11-25 23:50:11 +0100

    modalbox-err-wip

--- htdocs/scripts/konference.js
+++ htdocs/scripts/konference.js
@@ -1,261 +1,7 @@
 // STRICT MODE 
 'use strict';
 
-export const Content = () => {
-  return (
-    <div className="bg-gray-100">
-      <div className="relative px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-        <div className="absolute inset-x-0 top-0 items-center justify-center hidden overflow-hidden md:flex md:inset-y-0">
-          <svg
-            viewBox="0 0 88 88"
-            className="w-full max-w-screen-xl text-indigo-100"
-          >
-            <circle fill="currentColor" cx="44" cy="44" r="15.5" />
-            <circle
-              fillOpacity="0.2"
-              fill="currentColor"
-              cx="44"
-              cy="44"
-              r="44"
-            />
-            <circle
-              fillOpacity="0.2"
-              fill="currentColor"
-              cx="44"
-              cy="44"
-              r="37.5"
-            />
-            <circle
-              fillOpacity="0.3"
-              fill="currentColor"
-              cx="44"
-              cy="44"
-              r="29.5"
-            />
-            <circle
-              fillOpacity="0.3"
-              fill="currentColor"
-              cx="44"
-              cy="44"
-              r="22.5"
-            />
-          </svg>
-        </div>
 
-        <div className="relative grid gap-5 sm:grid-cols-2 lg:grid-cols-4">
-
-          <div className="flex flex-col justify-between overflow-hidden text-left transition-shadow duration-200 bg-white rounded shadow-xl group hover:shadow-2xl">
-            <div className="p-5">
-              <div className="flex items-center justify-center w-10 h-10 mb-4 rounded-full bg-indigo-50">
-                <svg
-                  className="w-8 h-8 text-deep-purple-accent-400"
-                  stroke="currentColor"
-                  viewBox="0 0 52 52"
-                >
-                  <polygon
-                    strokeWidth="3"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    fill="none"
-                    points="29 13 14 29 25 29 23 39 38 23 27 23"
-                  />
-                </svg>
-              </div>
-              <p className="mb-2 font-bold">Football Sports</p>
-              <p className="text-sm leading-5 text-gray-900">
-                Sed ut perspiciatis unde omnis iste. Lorem ipsum dolor sit amet,
-                consectetur adipiscing elit.
-              </p>
-            </div>
-            <div className="w-full h-1 ml-auto duration-300 origin-left transform scale-x-0 bg-deep-purple-accent-400 group-hover:scale-x-100" />
-          </div>
-
-          <div className="flex flex-col justify-between overflow-hidden text-left transition-shadow duration-200 bg-white rounded shadow-xl group hover:shadow-2xl">
-            <div className="p-5">
-              <div className="flex items-center justify-center w-10 h-10 mb-4 rounded-full bg-indigo-50">
-                <svg
-                  className="w-8 h-8 text-deep-purple-accent-400"
-                  stroke="currentColor"
-                  viewBox="0 0 52 52"
-                >
-                  <polygon
-                    strokeWidth="3"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    fill="none"
-                    points="29 13 14 29 25 29 23 39 38 23 27 23"
-                  />
-                </svg>
-              </div>
-              <p className="mb-2 font-bold">Bowling Sports</p>
-              <p className="text-sm leading-5 text-gray-900">
-                Disrupt inspire and think tank, social entrepreneur but
-                preliminary thinking think tank compelling.
-              </p>
-            </div>
-            <div className="w-full h-1 ml-auto duration-300 origin-left transform scale-x-0 bg-deep-purple-accent-400 group-hover:scale-x-100" />
-          </div>
-
-          <div className="flex flex-col justify-between overflow-hidden text-left transition-shadow duration-200 bg-white rounded shadow-xl group hover:shadow-2xl">
-            <div className="p-5">
-              <div className="flex items-center justify-center w-10 h-10 mb-4 rounded-full bg-indigo-50">
-                <svg
-                  className="w-8 h-8 text-deep-purple-accent-400"
-                  stroke="currentColor"
-                  viewBox="0 0 52 52"
-                >
-                  <polygon
-                    strokeWidth="3"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    fill="none"
-                    points="29 13 14 29 25 29 23 39 38 23 27 23"
-                  />
-                </svg>
-              </div>
-              <p className="mb-2 font-bold">Cycling Sports</p>
-              <p className="text-sm leading-5 text-gray-900">
-                A slice of heaven. O for awesome, this chocka full cuzzie is as
-                rip-off as a cracker.
-              </p>
-            </div>
-            <div className="w-full h-1 ml-auto duration-300 origin-left transform scale-x-0 bg-deep-purple-accent-400 group-hover:scale-x-100" />
-          </div>
-
-          <div className="flex flex-col justify-between overflow-hidden text-left transition-shadow duration-200 bg-white rounded shadow-xl group hover:shadow-2xl">
-            <div className="p-5">
-              <div className="flex items-center justify-center w-10 h-10 mb-4 rounded-full bg-indigo-50">
-                <svg
-                  className="w-8 h-8 text-deep-purple-accent-400"
-                  stroke="currentColor"
-                  viewBox="0 0 52 52"
-                >
-                  <polygon
-                    strokeWidth="3"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    fill="none"
-                    points="29 13 14 29 25 29 23 39 38 23 27 23"
-                  />
-                </svg>
-              </div>
-              <p className="mb-2 font-bold">Weight Lifting Sports</p>
-              <p className="text-sm leading-5 text-gray-900">
-                Meanwhile, in behind the bicycle shed, Hercules Morse, as big as
-                a horse.
-              </p>
-            </div>
-            <div className="w-full h-1 ml-auto duration-300 origin-left transform scale-x-0 bg-deep-purple-accent-400 group-hover:scale-x-100" />
-          </div>
-
-          <div className="flex flex-col justify-between overflow-hidden text-left transition-shadow duration-200 bg-white rounded shadow-xl group hover:shadow-2xl">
-            <div className="p-5">
-              <div className="flex items-center justify-center w-10 h-10 mb-4 rounded-full bg-indigo-50">
-                <svg
-                  className="w-8 h-8 text-deep-purple-accent-400"
-                  stroke="currentColor"
-                  viewBox="0 0 52 52"
-                >
-                  <polygon
-                    strokeWidth="3"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    fill="none"
-                    points="29 13 14 29 25 29 23 39 38 23 27 23"
-                  />
-                </svg>
-              </div>
-              <p className="mb-2 font-bold">Golf Sports</p>
-              <p className="text-sm leading-5 text-gray-900">
-                Disrupt inspire and think tank, social entrepreneur but
-                preliminary thinking think tank compelling.
-              </p>
-            </div>
-            <div className="w-full h-1 ml-auto duration-300 origin-left transform scale-x-0 bg-deep-purple-accent-400 group-hover:scale-x-100" />
-          </div>
-
-          <div className="flex flex-col justify-between overflow-hidden text-left transition-shadow duration-200 bg-white rounded shadow-xl group hover:shadow-2xl">
-            <div className="p-5">
-              <div className="flex items-center justify-center w-10 h-10 mb-4 rounded-full bg-indigo-50">
-                <svg
-                  className="w-8 h-8 text-deep-purple-accent-400"
-                  stroke="currentColor"
-                  viewBox="0 0 52 52"
-                >
-                  <polygon
-                    strokeWidth="3"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    fill="none"
-                    points="29 13 14 29 25 29 23 39 38 23 27 23"
-                  />
-                </svg>
-              </div>
-              <p className="mb-2 font-bold">Hockey Sports</p>
-              <p className="text-sm leading-5 text-gray-900">
-                A business big enough that it could be listed on the NASDAQ goes
-                belly up.
-              </p>
-            </div>
-            <div className="w-full h-1 ml-auto duration-300 origin-left transform scale-x-0 bg-deep-purple-accent-400 group-hover:scale-x-100" />
-          </div>
-
-          <div className="flex flex-col justify-between overflow-hidden text-left transition-shadow duration-200 bg-white rounded shadow-xl group hover:shadow-2xl">
-            <div className="p-5">
-              <div className="flex items-center justify-center w-10 h-10 mb-4 rounded-full bg-indigo-50">
-                <svg
-                  className="w-8 h-8 text-deep-purple-accent-400"
-                  stroke="currentColor"
-                  viewBox="0 0 52 52"
-                >
-                  <polygon
-                    strokeWidth="3"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    fill="none"
-                    points="29 13 14 29 25 29 23 39 38 23 27 23"
-                  />
-                </svg>
-              </div>
-              <p className="mb-2 font-bold">Shooting Sports</p>
-              <p className="text-sm leading-5 text-gray-900">
-                Lookout flogging bilge rat main sheet bilge water nipper fluke
-                to go on account heave down clap of thunder.
-              </p>
-            </div>
-            <div className="w-full h-1 ml-auto duration-300 origin-left transform scale-x-0 bg-deep-purple-accent-400 group-hover:scale-x-100" />
-          </div>
-          <div className="flex flex-col justify-between overflow-hidden text-left transition-shadow duration-200 bg-white rounded shadow-xl group hover:shadow-2xl">
-            <div className="p-5">
-              <div className="flex items-center justify-center w-10 h-10 mb-4 rounded-full bg-indigo-50">
-                <svg
-                  className="w-8 h-8 text-deep-purple-accent-400"
-                  stroke="currentColor"
-                  viewBox="0 0 52 52"
-                >
-                  <polygon
-                    strokeWidth="3"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    fill="none"
-                    points="29 13 14 29 25 29 23 39 38 23 27 23"
-                  />
-                </svg>
-              </div>
-              <p className="mb-2 font-bold">Martial Arts</p>
-              <p className="text-sm leading-5 text-gray-900">
-                Webtwo ipsum orkut reddit meebo skype vimeo jajah spock empressr
-                zimbra, mobly napster.
-              </p>
-            </div>
-            <div className="w-full h-1 ml-auto duration-300 origin-left transform scale-x-0 bg-deep-purple-accent-400 group-hover:scale-x-100" />
-          </div>
-
-        </div>
-      </div>
-    </div>
-  );
-};
 
 
 
@@ -263,21 +9,63 @@ export const Content = () => {
 // --------------------------------------------- Conference Modal Boxes (= popup window) -----------------------------------------------
 
 // NOT YET FUNCTIONAL
-/*
+
 // Get the modal
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn1 = document.getElementById("B1");
+var btn2 = document.getElementById("B2");
+var btn3 = document.getElementById("B3");
+var btn4 = document.getElementById("B4");
+var btn5 = document.getElementById("B5");
+var btn6 = document.getElementById("B6");
+var btn7 = document.getElementById("B7");
+var btn8 = document.getElementById("B8");
+var btn9 = document.getElementById("B9");
+var btn10 = document.getElementById("B10");
+var btn11 = document.getElementById("B11");
+var btn12 = document.getElementById("B12");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() 
-{
-  modal.style.display = "block";
-}
+btn1.onclick = function() 
+{ modal.style.display = "block";}
+
+btn2.onclick = function() 
+{ modal.style.display = "block";}
+
+btn3.onclick = function() 
+{ modal.style.display = "block";}
+
+btn4.onclick = function() 
+{ modal.style.display = "block";}
+
+btn5.onclick = function() 
+{ modal.style.display = "block";}
+
+btn6.onclick = function() 
+{ modal.style.display = "block";}
+
+btn7.onclick = function() 
+{ modal.style.display = "block";}
+
+btn8.onclick = function() 
+{ modal.style.display = "block";}
+
+btn9.onclick = function() 
+{ modal.style.display = "block";}
+
+btn10.onclick = function() 
+{ modal.style.display = "block";}
+
+btn11.onclick = function() 
+{ modal.style.display = "block";}
+
+btn12.onclick = function() 
+{ modal.style.display = "block";}
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() 
@@ -293,4 +81,3 @@ window.onclick = function(event)
     modal.style.display = "none";
   }
 }
-*/
