e4c3eafed0684830e4f94b6fb31a5691011a03bf
VBatto <vdkstar@gmail.com>
2021-11-25 21:01:55 +0100

    profile-visual

---
+++ htdocs/scripts/general.js
@@ -0,0 +1,13 @@
+/*
+// TODO: make fn, ADD THIS SCRIPT TO ALL HTML FILES, test
+function loggedIn (token)
+{
+    if( ... )
+    {
+        ..
+        return true;
+    }
+
+    return false;
+}
+*/
