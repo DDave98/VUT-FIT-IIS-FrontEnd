505efdcf6a41b92609c3e9a8c3f67b1b165f7768
VBatto <vdkstar@gmail.com>
2021-11-29 20:20:53 +0100

    trtry

--- htdocs/scripts/general.js
+++ htdocs/scripts/general.js
@@ -46,8 +46,6 @@ function getCookie(attr)
 {
   // attr can be: "login"    "lvl"    "expire"     "accesstoken" (token)
   //--------
-
-/*
   
   var name = attr + "=";
 
@@ -71,28 +69,7 @@ function getCookie(attr)
     }
   }
   return "";
-  */
-  // ---------------------
-    
-  
-    let cookie = {};
-    document.cookie.split('; ').forEach(
-        function(el) 
-        {
-            let [key,value] = el.split('=');
-            cookie[key.trim()] = value;
-
-            if(key === attr)
-            {
-    
-              return cookie[attr];
-            }
-        }
-        
-    )
-
-    
-    
+   
 }
 
 
